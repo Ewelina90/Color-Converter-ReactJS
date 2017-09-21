1243b6a8581e38392a7d0c755f531a6db382873d
Ewelina Dziura <ewedziura@gmail.com>
2017-09-21 20:55:13 +0200

    create components template

---
+++ js/app.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded',function(){
+
+    class ConvertedColors extends React.Component {
+
+        render() {
+
+        }
+    }
+
+    class ColorBackground extends React.Component {
+
+        render() {
+
+        }
+    }
+
+    class ColorInput extends React.Component {
+
+        render() {
+
+        }
+    }
+
+    class App extends React.Component {
+
+        render() {
+            
+        }
+    }
+
+    ReactDOM.render(
+        <App />,
+        document.getElementById('app')
+    );
+});
